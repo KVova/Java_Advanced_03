19ed2fa34ee8de1cc719c94866f8944fe2d09ca3
K_Vova <karmazynvolodymyr>
2020-05-09 15:53:58 +0300

    Log4j

---
+++ lesson-03-JavaAdvance/src/main/java/volodymyr/Application.java
@@ -0,0 +1,37 @@
+package volodymyr;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public class Application {
+
+	private static Logger LOG = Logger.getLogger(Application.class);
+	
+	public static void main(String[] args) {
+		
+		logWithDomConfigurator();
+		logWithBasicConfigurator();
+		
+	}
+	
+public static void logWithDomConfigurator() {
+		
+		DOMConfigurator.configure("LoggerConfig.xml");
+		LOG.debug("DEBUG Logger message");
+		LOG.info("INFO Logger message");
+		LOG.warn("WARN Logger message");
+		
+		LOG.error("ERROR Logger message in you project");	
+	}
+
+public static void logWithBasicConfigurator() {
+	
+	BasicConfigurator.configure();
+	LOG.debug("DEBUG Logger message of project");
+	LOG.info("INFO Logger message of project");
+	LOG.warn("WARN Logger message of project");
+	
+	LOG.error("ERROR Logger message");
+	}
+}
